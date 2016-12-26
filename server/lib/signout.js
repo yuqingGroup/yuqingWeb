d3f110269baf5ad032b92901293c9ae26c28bc75
wei gang <15594818350@163.com>
2016-12-26 09:29:50 +0800

    test

--- server/lib/signout.js
+++
@@ -1,7 +0,0 @@
-exports.init = function (request, response) {
-	console.log("logout-------------------------------------------------")
-    request.logout();
-    response.status(200).json({
-        status: "Bye!"
-    });
-};
